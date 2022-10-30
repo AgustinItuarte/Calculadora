f48a837effd4649bc200a3423a764fa36b9b27f1
AgustinItuarte <agustin242536@gmail.com>
2022-10-29 21:09:24 -0300

    Añadidos botones y pantalla de la calculadora al html

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
+pantalla = document.querySelector('#pantalla');
+botones = document.querySelectorAll('button');
+num1 = document.querySelector('.numero1').value = 1;
+num2 = document.querySelector('.numero2').value = 2;
+num3 = document.querySelector('.numero3').value = 3;
+num4 = document.querySelector('.numero4').value = 4;
+num5 = document.querySelector('.numero5').value = 5;
+num6 = document.querySelector('.numero6').value = 6;
+num7 = document.querySelector('.numero7').value = 7;
+num8 = document.querySelector('.numero8').value = 8;
+num9 = document.querySelector('.numero9').value = 9;
+num0 = document.querySelector('.numero0').value = 0;
+// pantalla.value = num1;
+
+botones.addEventListener('click', () => {
+    if (botones.class == "numero1") {
+        console.log('hola')
+    }
+})
+
 function suma(num1, num2) {
     return num1 + num2;
 }
