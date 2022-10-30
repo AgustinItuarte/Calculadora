26e7356b060e3e767f7cb170f8d4b034dd661bbc
AgustinItuarte <agustin242536@gmail.com>
2022-10-29 23:05:06 -0300

    Separado el valor del operador

--- scripts.js
+++ scripts.js
@@ -1,22 +1,33 @@
-pantalla = document.querySelector('#pantalla');
-botones = document.querySelectorAll('button');
-num1 = document.querySelector('.numero1').value = 1;
-num2 = document.querySelector('.numero2').value = 2;
-num3 = document.querySelector('.numero3').value = 3;
-num4 = document.querySelector('.numero4').value = 4;
-num5 = document.querySelector('.numero5').value = 5;
-num6 = document.querySelector('.numero6').value = 6;
-num7 = document.querySelector('.numero7').value = 7;
-num8 = document.querySelector('.numero8').value = 8;
-num9 = document.querySelector('.numero9').value = 9;
-num0 = document.querySelector('.numero0').value = 0;
+const pantalla = document.querySelector('#pantalla');
+const botones = document.querySelectorAll('button');
+
+const num1 = document.querySelector('.numero1').value = 1;
+const num2 = document.querySelector('.numero2').value = 2;
+const num3 = document.querySelector('.numero3').value = 3;
+const num4 = document.querySelector('.numero4').value = 4;
+const num5 = document.querySelector('.numero5').value = 5;
+const num6 = document.querySelector('.numero6').value = 6;
+const num7 = document.querySelector('.numero7').value = 7;
+const num8 = document.querySelector('.numero8').value = 8;
+const num9 = document.querySelector('.numero9').value = 9;
+const num0 = document.querySelector('.numero0').value = 0;
+
+const sumaBoton = document.querySelector('.suma').value = " +";
 // pantalla.value = num1;
 
-botones.addEventListener('click', () => {
-    if (botones.class == "numero1") {
-        console.log('hola')
+// botones.addEventListener('click', () => {
+//     if (botones.class == "numero1") {
+//         console.log('hola')
+//     }
+// })
+botones.forEach(boton => boton.addEventListener('click', () => {
+    pantalla.value += boton.value;
+    let pantallaValor = pantalla.value;
+    if (pantallaValor.indexOf('+') > -1) {
+        let operadorStr = pantallaValor.split(" ").pop();
+        console.log(operadorStr);
     }
-})
+}))
 
 function suma(num1, num2) {
     return num1 + num2;
@@ -46,4 +57,4 @@ function operar(num1, num2, operador) {
     }
 }
 
-console.log(operar(5, 1, "/"))
+// console.log(operar(5, 1, "/"))
